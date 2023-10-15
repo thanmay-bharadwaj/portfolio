fafe380431b94eb52b3d1efc000f857f01cb2034
Thanmay Bharadwaj J S <thanmaybharadwaj.js@gmail.com>
2023-10-15 21:36:39 +0530

    personal website

---
+++ src/data/constants.js
@@ -0,0 +1,297 @@
+export const Bio = {
+    name: "Thanmay Bharadwaj J S",
+    roles: [
+      "Software Developer",
+      "FullStack Developer",
+      "UI/UX Designer",
+      "Programmer",
+    ],
+    description:
+      "I'm an enthusiastic and adaptable individual who thrives on tackling fresh challenges. Driven by a love for learning, I'm committed to producing top-notch outcomes, armed with a constructive outlook and a determination for significant contributions and accomplishments.",
+    github: "https://github.com/thanmay-bharadwaj",
+    resume:
+      "https://drive.google.com/file/d/1t7MtbJhrgRWWs7q9_FcrHm3hIiNcrcpn/view?usp=sharing",
+    linkedin: "https://www.linkedin.com/in/thanmay-bharadwaj-j-s/",
+    mail: "mailto:thanmaybharadwaj.js@gmail.com",
+    insta: "https://www.instagram.com/thanmay_bharadwaj",
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Angular",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/angular%20logo.png?alt=media&token=b814453f-eff4-44df-8418-3e3b9d14d507&_gl=1*e4o34*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MDM3MC41MC4wLjA.",
+        },
+        {
+          name: "Jasmine",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/jasmine-white-horizontal.png?alt=media&token=7c71b493-4b38-48ba-b9d4-83c16843b842&_gl=1*ar8zdz*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MDY1Mi41My4wLjA.",
+        },
+        {
+          name: "Karma",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/karma.png?alt=media&token=388faaf1-d70e-45fe-9b20-c82342ac6864&_gl=1*1dtkuuu*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MDg0NC4xNS4wLjA.",
+        },
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/python.png?alt=media&token=4d816be2-ff8d-4ed1-afd9-33d33186874a&_gl=1*1vxirne*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTA5NC40OC4wLjA.",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/mysql.png?alt=media&token=3b1c7eeb-cfef-4b34-aabd-86933a05a1cb&_gl=1*tpiewa*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTE4Ny41My4wLjA.",
+        },
+        {
+          name: "Postgresql",
+          image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        },
+        {
+          name: "MongoDB",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/mongodb.png?alt=media&token=6f0063ee-af97-40aa-b204-c0077f9e8423&_gl=1*1w7fx9*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTI2Ny41My4wLjA.",
+        },
+        {
+          name: "Django",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/django.png?alt=media&token=7a0a33ec-5538-46b8-a4f2-2affde254732&_gl=1*19blup*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTM5Ni41My4wLjA.",
+        },
+        {
+          name: "Firebase",
+          image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+        },
+        {
+          name: "C",
+          image: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/C%20prog.jpg?alt=media&token=1cd39001-81ec-4aa4-9b98-4dc87a215d5b&_gl=1*1ill63j*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTU5Ni41NS4wLjA.",
+        },
+        {
+          name: "C++",
+          image: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/C%2B%2B.png?alt=media&token=a556b508-78dd-47cc-a4ed-c6c2bd797f2d&_gl=1*1uwv17x*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTc2MS4zNy4wLjA.",
+        },
+      ],
+    },
+
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "Docker",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/Docker.png?alt=media&token=0df16f7a-1cc9-415a-9697-6e7c38e147d7&_gl=1*nhjslb*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MjAyMy41MS4wLjA.",
+        },
+        {
+          name: "Netlify",
+          image:
+            "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+        },
+        {
+          name: "Adobe XD",
+          image:
+            "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+        },
+        {
+          name: "Adobe Photoshop",
+          image:
+            "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/photoshop.png?alt=media&token=57884d75-ab1b-4794-9f12-a8f5ad1fad8e&_gl=1*1nf3lmp*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI3MTkyNS41MC4wLjA.",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/kenscio_logo.png?alt=media&token=a2aeec97-4b28-404e-8fe4-32023e8172ce&_gl=1*vvnfx9*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5Njk0MjMzOC4xLjEuMTY5Njk0MjYwMC42MC4wLjA.",
+      role: "Software Engineer - Trainee",
+      company: "Kenscio - Aghreni Technologies Pvt. Ltd.",
+      date: "July 2023 - Present",
+      desc: "Working on Kenscio Platforms, managing backend, and streamlining the process with automation.",
+      skills: [
+        "SQL",
+        "Docker",
+        "Python",
+        "Bash Scripting",
+        "Node Js",
+        "JavaScript",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/employment.png?alt=media&token=ee8293b1-f58f-4ede-9e53-d16b22e26f69&_gl=1*1ic5hfs*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI2OTk3OC41MC4wLjA.",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/kenscio_logo.png?alt=media&token=a2aeec97-4b28-404e-8fe4-32023e8172ce&_gl=1*vvnfx9*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5Njk0MjMzOC4xLjEuMTY5Njk0MjYwMC42MC4wLjA.",
+      role: "Software Engineer Intern",
+      company: "Kenscio - Aghreni Technologies Pvt. Ltd.",
+      date: "November 2022 - June 2023",
+      desc: "Worked on the backend of the web application using Python, Redis Database and Postman.",
+      skills: [
+        "Redis Databse",
+        "Postman",
+        "Python",
+      ],
+      doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/internship_letter.png?alt=media&token=03e19af1-1666-43a4-93cb-cd5cbf88e2d4&_gl=1*zs8fcg*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzI2OTcyNC4zLjEuMTY5NzI2OTc4Mi4yLjAuMA.."
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/JIT.png?alt=media&token=dfc4af21-304a-4f97-a080-a316377d3622&_gl=1*1iox75d*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5Njk0MjMzOC4xLjEuMTY5Njk0MjY3MS42MC4wLjA.",
+      school: "Jyothy Institute of Technology, Bengaluru",
+      date: "2019 - 2023",
+      grade: "7.89 CGPA",
+      desc: "I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Engineering - B.E, Information Science and Engineering",
+    },
+    {
+      id: 1,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/Sadhana-Logo-PNG.png?alt=media&token=c05ee790-14d1-4939-acac-c2445751d670&_gl=1*1udb34x*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5Njk0MjMzOC4xLjEuMTY5Njk0MzEyMS42MC4wLjA..",
+      school: "Sadhana PU College, Bengaluru",
+      date: "Apr 2019 - Apr 2025",
+      grade: "85.16%",
+      desc: "I completed my Pre-University College(PUC) education at Sadhana PU College, Bengaluru, where I studied Science with Computer Science.",
+      degree: "PUC, Science with Computer",
+    },
+    {
+      id: 2,
+      img: "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/prarthana.png?alt=media&token=c97f6f2c-b8b5-46fc-84ab-9f043b86e25b&_gl=1*87ldmt*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5Njk0MjMzOC4xLjEuMTY5Njk0MzA3My40OC4wLjA.",
+      school: "Prarthana School, Bengaluru",
+      date: "2017",
+      grade: "9.8 CGPA",
+      desc: "I completed my class 10 education at Prarthana School, Bengaluru, where I studied Science with Computer Application.",
+      degree: "CBSE(X), Science with Computer",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 9,
+      title: "ML based Diabetics wound recognition using Poly Approximate Algorithm (Final Year Major Project)",
+      description:
+        "Developed a novel solution using Python, Poly Approximation Algorithm, and YOLO v8 to tackle the problem of diabetic wound recognition.Implemented machine learning algorithms to accurately identify and classify diabetic wounds, leveraging the power of computer vision.Demonstrated strong technical skills in programming, algorithm design, and utilizing cutting-edge technologies for real-world healthcare applications.",
+      tags: [
+        "Python",
+        "Machine Learning",
+        "YOLO v8",
+        "labellimg",
+        "Poly Approximation Algorithm",
+      ],
+      category: "web app",
+    },
+    {
+      id: 1,
+      title: "Weather App",
+      description:
+        "Developed a weather web app using Angular, providing real-time weather updates and forecasts for users. The app delivers accurate weather data from various sources.",
+      tags: [
+        "Angular",
+        "TypeScript",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      category: "web app",
+    },
+    {
+      id: 1,
+      title: "Personal Diary Application using Django Framework",
+      description:
+        "Personal Diary web application using Django Framework (SQLite) for backend and HTML, CSS, JavaScript for frontend.",
+      tags: [
+        "Angular",
+        "TypeScript",
+        "HTML",
+        "CSS",
+        "JavaScript",
+      ],
+      category: "web app",
+    },
+    {
+      id: 3,
+      title: "Programming for Everybody (Getting Started with Python)",
+      description:
+        "An online course authorized by University of Michigan and offered through Coursera",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/Certificate-1.png?alt=media&token=8e7f0d24-9e6a-4882-bc8b-4ccf224a31a4&_gl=1*1vr0hu9*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzMzMTE5Ni40LjEuMTY5NzMzMjk1OS4yNS4wLjA.",
+      tags: ["Kotlin", "MVVM", "Room Database", "Google Drive Cloud API"],
+      category: "android app",
+      github: "https://github.com/rishavchanda/Buckoid-Android-App",
+      webapp: "https://play.google.com/store/apps/details?id=com.rishav.buckoid",
+    },
+    {
+      id: 10,
+      title: "Python Data Structures",
+      description:
+        "An online course authorized by University of Michigan and offered through Coursera",
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/portfolio-assets-71f61.appspot.com/o/Certificate-2.png?alt=media&token=4cc429ac-bb2e-4ab3-8fe7-640ab7e5b540&_gl=1*1jleu69*_ga*NDkyMTk3MjI5LjE2OTY5NDIzMzk.*_ga_CW55HF8NVT*MTY5NzMzMTE5Ni40LjEuMTY5NzMzMzAyNi42MC4wLjA.",
+      tags: ["React Native", "JavaScript", "Axios"],
+      category: "android app",
+      github: "https://github.com/rishavchanda/Job-finder-App",
+      webapp: "https://github.com/rishavchanda/Job-finder-App",
+    },
+  ];
+
+  export const TimeLineData = [
+    { year: 2017, text: "Started my journey" },
+    { year: 2018, text: "Worked as a freelance developer" },
+    { year: 2019, text: "Founded JavaScript Mastery" },
+    { year: 2020, text: "Shared my projects with the world" },
+    { year: 2021, text: "Started my own platform" },
+  ];
+  
